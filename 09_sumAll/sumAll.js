5cfe3989548fa50a16fa715f22cafb444ba3be27
AidanDjalins <aidan.djalins@binus.ac.id>
2025-10-23 22:16:17 +0700

    Comleted '09-sumAll' exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
 
+    let result = 0;
+
+    if  (lowerBound > upperBound) { 
+        [lowerBound, upperBound] = [upperBound, lowerBound];
+    }
+
+    if (lowerBound < 0 || !upperBound < 0) return 'ERROR';
+    
+    if (!Number.isInteger(lowerBound) || !Number.isInteger(upperBound)) return "ERROR";
+
+    for(let i = lowerBound; i <= upperBound; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
